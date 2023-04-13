0c36017f7f511346c5cad00d3666d6b12aa19e8f
Abhishek kumar <kumar.abhi021@gmail.com>
2023-04-13 20:39:19 +0530

    inital route set up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-        <>
-        <div className="text-3xl text-red-400">this is Header</div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="text-3xl text-red-400">this is Header</div>
+      <div className="flex justify-evenly">
+        <Link to="/cart">Cart</Link>
+        <Link to="/">Home</Link>
+        <Link to="/item">Item</Link>
+        <Link to="/contact-us">Contact Us</Link>
+      </div>
+    </>
+  );
+};
 
-export default Header
+export default Header;
